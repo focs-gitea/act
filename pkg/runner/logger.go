e0b1a11448bbf2dd57791db2daa37ff2f45f8b8c
a1012112796 <1012112796@qq.com>
2023-03-07 09:56:31 +0000

    use new way for log level configing`
    Signed-off-by: a1012112796 <1012112796@qq.com>

--- pkg/runner/logger.go
+++ pkg/runner/logger.go
@@ -84,7 +84,11 @@ func WithJobLogger(ctx context.Context, jobID string, jobName string, config *Co
 	}
 	logger.SetFormatter(formatter)
 	logger.SetOutput(os.Stdout)
-	logger.SetLevel(logrus.GetLevel())
+	if config.JobLoggerLevel != nil {
+		logger.SetLevel(*config.JobLoggerLevel)
+	} else {
+		logger.SetLevel(logrus.TraceLevel)
+	}
 	rtn := logger.WithFields(logrus.Fields{
 		"job":    jobName,
 		"jobID":  jobID,
